9fd173540e946f6a9053c70ef1aae43c34346177
cecoweee <newcecoweee@gmail.com>
2018-06-04 16:16:56 +0300

    Users things

--- Our blog_forum/server.py
+++ Our blog_forum/server.py
@@ -47,19 +47,30 @@ def get_recipes():
 
 @app.route('/users/add', methods=('GET', 'POST'))
 def add_user():
+    error = None
     g.active_url = '/users/add'
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
-        db.execute(
-            'INSERT INTO user (username, password) VALUES (?, ?)',
-            (username, password)
-        )
-        db.commit()
-        return redirect('users')
-    else:
-        return render_template('user/add-user.html')
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+                'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = 'User {0} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, password)
+            )
+            db.commit()
+            return redirect('users')
+
+    return render_template('user/add-user.html', error=error)
 
 
 # users = get_all_users()
